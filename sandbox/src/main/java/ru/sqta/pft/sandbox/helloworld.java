51797566da63808a28d2f600c808ac88fb6056cd
manhis <59974019+manhis@users.noreply.github.com>
2020-02-19 08:58:17 +0300

    Изменены настройки форматирования проекта

--- sandbox/src/main/java/ru/sqta/pft/sandbox/helloworld.java
+++ sandbox/src/main/java/ru/sqta/pft/sandbox/helloworld.java
@@ -1,7 +1,10 @@
 package ru.sqta.pft.sandbox;
 
 public class helloworld {
-public static void main (String[] args) {
-System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
-}
+
+
